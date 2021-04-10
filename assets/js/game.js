e4be8daef4d8fbfde7c418125457c039ebe467eb
yalitzab <yalitzabueno@gmail.com>
2021-04-10 12:38:49 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -58,11 +58,20 @@ var fight = function () {
     } else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+
     // if player choses to skip
   } else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
-  } else {
-    window.alert("You need to choose a valid option. Try again!");
+    // confirm player wants to skip
+    var confirmskip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
+  }
+  // if no (false), ask question again by running fight() again
+  else {
+    fight();
   }
 };
 
