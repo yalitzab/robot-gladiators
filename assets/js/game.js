922d9324d577a4f45118a79f9f3151a58b0342a2
yalitzab <yalitzabueno@gmail.com>
2021-04-10 11:54:06 -0700

    add conditions to playerhealth

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,3 @@
-
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
@@ -9,17 +8,44 @@ var enemyAttack = 12;
 
 // console.log(playerName, playerAttack, playerHealth);
 
-var fight = function() {
-// Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+var fight = function () {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
 
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-
+  enemyHealth = enemyHealth - playerAttack;
+  
   // Log a resulting message to the console so we know that it worked.
+  console.log(
+    playerName +
+      " attacked " +
+      enemyName +
+      ". " +
+      enemyName +
+      " now has " +
+      enemyHealth +
+      " health remaining."
+  );
 
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth = playerHealth - enemyAttack;
 
   // Log a resulting message to the console so we know that it worked.
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+
+  // check to see if the value of the playerHealth variable is greater than 0
+  if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+  }
 };
 
-    fight()
+fight();
